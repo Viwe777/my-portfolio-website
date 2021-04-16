65890895988872faa8a007d47db95edca260b7b5
Viwe777 <82093843+Viwe777@users.noreply.github.com>
2021-04-16 14:12:40 +0200

    Add files via upload

---
+++ chapt15/chapt15.js
@@ -0,0 +1,12 @@
+function displayDate() {
+    // todo: display the current date in the todaysdate div
+}
+
+function displayActivity() {
+    // todo: find out the day of the week.
+
+    /* todo: set a variable, called youShould, with a different
+       string based on what day of the week it is. */
+
+    // todo: output the value of youShould into the thingToDo div
+}
